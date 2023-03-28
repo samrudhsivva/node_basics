721ee0fc96d5eaf20c1885c8f6c2552cacc5835a
Samrudh Sivva <samrudh5000@gmail.com>
2023-03-28 23:44:18 +0530

    Add files via upload
    superhero and supervillain modules

---
+++ nodefiles/index.js
@@ -0,0 +1,9 @@
+var superheroes= require("superheroes");
+var supervillains=require("supervillains");
+console.log(superheroes.random());
+
+//console.log(superheroes.all);
+
+console.log(" defeats ");
+var supervillain=supervillains.random();
+console.log(supervillain);
